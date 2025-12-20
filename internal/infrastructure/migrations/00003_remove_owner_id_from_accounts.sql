0aa91a0834c96b141e0e5e60b1bc26daa8c5da8e
howard127 <contact@xhw.jp>
2025-12-20 18:23:37 +0900

    fix

--- internal/infrastructure/migrations/00003_remove_owner_id_from_accounts.sql
+++ internal/infrastructure/migrations/00003_remove_owner_id_from_accounts.sql
@@ -1,11 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE accounts DROP INDEX idx_owner_id;
+-- +goose StatementEnd
+-- +goose StatementBegin
 ALTER TABLE accounts DROP COLUMN owner_id;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE accounts ADD COLUMN owner_id BINARY(16) NOT NULL UNIQUE AFTER id;
+-- +goose StatementEnd
+-- +goose StatementBegin
 ALTER TABLE accounts ADD INDEX idx_owner_id (owner_id);
 -- +goose StatementEnd
